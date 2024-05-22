220bf54b54fe2338d426b2b0fa4af9a297971628
César <143505457+LetoKurosawa@users.noreply.github.com>
2024-05-22 13:14:12 +0100

    import words list and art

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,79 +1,15 @@
 import secrets
+from art import logo
+from art import stages
+from words import word_list
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["aardvark", "baboon", "camel"]
 chosen_word = secrets.choice(word_list)
 finish_game = False
 lives = 6
 
-print(f'Pssst, the solution is {chosen_word}.')
-print('''
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/      
-      ''')
+print(logo)
 
+print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 for letter in chosen_word:
@@ -82,12 +18,15 @@ for letter in chosen_word:
 while not finish_game:
     guess = input("Guess a letter: ").lower()
     
+    if guess in display:
+            print(f"You've already guessed {guess}, chose another letter!")
+    
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             display[i] = guess
-            
-    
+
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life!")
         lives -= 1
         if lives == 0:
             print("You Lost!")
