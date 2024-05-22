7fc6098c517477833f8fd183fbf0b332ed768702
César <143505457+LetoKurosawa@users.noreply.github.com>
2024-05-22 23:21:12 +0100

    add logo and restart

--- caesar_cypher/caesar_cypher.py
+++ caesar_cypher/caesar_cypher.py
@@ -1,21 +1,32 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
 
-def caesar(text, shift, direction):
-    coded = ""
-    if direction == "encode":
-        for letter in  text:
-            coded += "".join(alphabet[(alphabet.index(letter) + shift) % 26])
-        print(f"The encoded text is {coded}")
-    if direction == "decode":
-        for letter in text:
-            coded += "".join(alphabet[(alphabet.index(letter) - shift) % 26])
-        print(f"The decoded text is {coded}")
-  
+while True:
 
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-caesar(text, shift, direction)
+    def caesar(text, shift, direction):
+        coded = ""
+        if direction == "encode":
+            for letter in  text:
+                coded += "".join(alphabet[(alphabet.index(letter) + shift) % 26])
+            print(f"The encoded text is {coded}")
+        if direction == "decode":
+            for letter in text:
+                coded += "".join(alphabet[(alphabet.index(letter) - shift) % 26])
+            print(f"The decoded text is {coded}")
+        
+    caesar(text, shift, direction)
 
+    answer = input("Type 'yes' if you want to do another operation. Otherwise type 'no'\n")
+
+    if answer.lower() == "yes":
+        continue
+    elif answer.lower() == 'no':
+        print("Goodbye")
+        break    
