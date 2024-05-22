676af6661d9c2cb106aa446fdd826aa51de229f8
César <143505457+LetoKurosawa@users.noreply.github.com>
2024-05-22 11:16:02 +0100

    add player guess

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,17 +1,19 @@
 import secrets
-#Step 1 
 
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = secrets.choice(word_list)
-print(chosen_word)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for letter in chosen_word:
+    display.append("_")
+
 guess = input("Guess a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chosen_word:
-    if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
+for i in range(len(chosen_word)):
+    if guess == chosen_word[i]:
+       display[i] = guess 
+
+print(display)
+
