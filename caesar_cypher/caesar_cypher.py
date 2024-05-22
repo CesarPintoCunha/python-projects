43abe7ae055a247734a110bd816fe3e5eb23497c
César <143505457+LetoKurosawa@users.noreply.github.com>
2024-05-22 23:53:56 +0100

    remove function from loop

--- caesar_cypher/caesar_cypher.py
+++ caesar_cypher/caesar_cypher.py
@@ -4,23 +4,29 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 print(logo)
 
+def caesar(text, shift, direction):
+    coded = ""
+    if direction == "encode":
+        for letter in  text:
+            if letter in alphabet:
+                coded += "".join(alphabet[(alphabet.index(letter) + shift) % 26])
+            else:
+                coded += letter
+        print(f"The encoded text is {coded}")
+    if direction == "decode":
+        for letter in text:
+            if letter in alphabet:
+                coded += "".join(alphabet[(alphabet.index(letter) - shift) % 26])
+            else:
+                coded += letter
+        print(f"The decoded text is {coded}")
+
 while True:
 
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
-    def caesar(text, shift, direction):
-        coded = ""
-        if direction == "encode":
-            for letter in  text:
-                coded += "".join(alphabet[(alphabet.index(letter) + shift) % 26])
-            print(f"The encoded text is {coded}")
-        if direction == "decode":
-            for letter in text:
-                coded += "".join(alphabet[(alphabet.index(letter) - shift) % 26])
-            print(f"The decoded text is {coded}")
-        
     caesar(text, shift, direction)
 
     answer = input("Type 'yes' if you want to do another operation. Otherwise type 'no'\n")
