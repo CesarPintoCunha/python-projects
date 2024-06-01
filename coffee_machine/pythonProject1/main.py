aeb7b15d4b5a0737e01e8e44fcd017d16c9a2765
César <cesarpintocunha@gmail.com>
2024-06-01 12:53:03 +0100

    add payment and resources management

--- coffee_machine/pythonProject1/main.py
+++ coffee_machine/pythonProject1/main.py
@@ -28,34 +28,29 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
-    "money": 100,
 }
+
+money = 100
+
 
 def process_choice(customer_choice):
     if customer_choice == "off":
         print("Shutting down")
     elif customer_choice == "report":
-        for item in resources:
-            print(item, ":", resources[item])
-    elif customer_choice == "espresso":
-        if resources["water"] < MENU["espresso"]["ingredients"]["water"]:
-            return print("Sorry there is not enough water.")
-        elif resources["coffee"] < MENU["espresso"]["ingredients"]["coffee"]:
-            return print("Sorry there is not enough coffee")
-    elif customer_choice == "latte":
-        if resources["water"] < MENU["latte"]["ingredients"]["water"]:
-            return print("Sorry there is not enough water.")
-        elif resources["milk"] < MENU["latte"]["ingredients"]["milk"]:
-            return print("Sorry there is not enough milk")
-        elif resources["coffee"] < MENU["latte"]["ingredients"]["coffee"]:
-            return print("Sorry there is not enough coffee")
-    elif customer_choice == "cappuccino":
-        if resources["water"] < MENU["cappuccino"]["ingredients"]["water"]:
-            return print("Sorry there is not enough water.")
-        elif resources["milk"] < MENU["cappuccino"]["ingredients"]["milk"]:
-            return print("Sorry there is not enough milk")
-        elif resources["coffee"] < MENU["cappuccino"]["ingredients"]["coffee"]:
-            return print("Sorry there is not enough coffee")
+        print(f"Water: {resources["water"]}ml")
+        print(f"Milk: {resources["milk"]}ml")
+        print(f"Coffee: {resources["coffee"]}g")
+        print(f"Money: {money}€")
+    elif customer_choice == "espresso" or customer_choice == "latte" or customer_choice == "cappuccino":
+        drink = MENU[customer_choice]["ingredients"]
+        for item in drink:
+            if drink[item] > resources[item]:
+                print(f"Sorry, there is not enough {item}.")
+                return False
+
+            return True
+
+
 
 def charge(customer_choice):
     print("Please insert coins.")
@@ -64,9 +59,20 @@ def charge(customer_choice):
     euro20 = int(input("how many .20 cents coins?"))
     euro10 = int(input("how many .10 cents coins?"))
     total_client = euro_1 + (0.5 * euro50) + (0.2 * euro20) + (0.10 * euro10)
+    profit = MENU[customer_choice]["cost"]
+    change = round(total_client - MENU[customer_choice]["cost"], 2)
     if total_client < MENU[customer_choice]["cost"]:
         print("Sorry that's not enough money. Money refunded.")
+    elif total_client >= MENU[customer_choice]["cost"]:
+        print(f"Here is {change}€ in change.")
+        print(f"Here is your {customer_choice} ☕️. Enjoy!")
+        global money
+        money += profit
 
+
+def resources_used(ingredients):
+    for item in ingredients:
+        resources[item] -= ingredients[item]
 
 
 machine_on = True
@@ -75,9 +81,12 @@ while machine_on:
     customer_choice = input("What would you like? (espresso/latte/cappuccino):")
     process_choice(customer_choice)
     if customer_choice == "espresso" or customer_choice == "latte" or customer_choice == "cappuccino":
-        charge(customer_choice)
+        if process_choice(customer_choice):
+            charge(customer_choice)
+            resources_used(MENU[customer_choice]["ingredients"])
     if customer_choice == "off":
         machine_on = False
+ 
 
 
 
